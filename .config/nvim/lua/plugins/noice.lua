cc09d418074caf2bbae1d500e1b73ab4aea65a87
Marco Castellanos <marcopoulut@gmail.com>
2025-11-04 20:12:52 -0800

    new noice plugings

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,67 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    lsp = {
+      progress = { enabled = true }, -- show LSP progress messages
+      hover = { enabled = true }, -- custom hover window
+      signature = { enabled = true }, -- custom signature help
+      message = { enabled = true }, -- show LSP messages
+    },
+    presets = {
+      bottom_search = true, -- position search bar at the bottom
+      command_palette = true, -- align cmdline and popupmenu together
+      long_message_to_split = true, -- send long messages to a split window
+      inc_rename = false, -- integrate with inc-rename.nvim
+      lsp_doc_border = true, -- add border to hover/signature windows
+    },
+    cmdline = {
+      enabled = true,
+      view = "cmdline_popup", -- "cmdline" (bottom) or "cmdline_popup" (floating)
+      format = {
+        cmdline = { icon = "" },
+        search_down = { icon = "⌄" },
+        search_up = { icon = "⌃" },
+      },
+    },
+    messages = {
+      enabled = true,
+      view = "mini", -- could be "mini", "notify", or "popup"
+      view_error = "notify",
+      view_warn = "notify",
+      view_history = "messages",
+      view_search = "virtualtext",
+    },
+    notify = {
+      enabled = true,
+      view = "notify",
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = "60%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+      },
+    },
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+}
