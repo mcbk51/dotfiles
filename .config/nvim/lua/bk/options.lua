43bcd71ef2f64068a4fe247b40a88829a4235512
Marco Castellanos <marcopoulut@gmail.com>
2025-11-09 18:33:38 -0800

    config yank highlight

--- .config/nvim/lua/bk/options.lua
+++ .config/nvim/lua/bk/options.lua
@@ -7,6 +7,14 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) texg",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {clear = true}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
 -- Apply immediately as well
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
