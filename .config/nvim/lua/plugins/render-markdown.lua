0e8c8dfe0f46a28d8a1423ee904a0c168a99e7d3
Marco Castellanos <marcopoulut@gmail.com>
2025-11-11 13:09:22 -0800

    fixing bug

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -1,27 +1,10 @@
 -- plugins/render-markdown.lua
 return {
-  "MeanderingProgrammer/markdown.nvim",
+ 'MeanderingProgrammer/render-markdown.nvim',
   lazy = true,
-  name = "render-markdown",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  ft = { "markdown" },
-  config = function()
-    local render_markdown = require "render-markdown"
-    render_markdown.setup(require "configs.render-markdown")
-
-    -- Auto-toggle rendering based on mode
-    vim.api.nvim_create_autocmd("InsertEnter", {
-      pattern = "*.md",
-      callback = function()
-        render_markdown.disable()
-      end,
-    })
-
-    vim.api.nvim_create_autocmd("InsertLeave", {
-      pattern = "*.md",
-      callback = function()
-        render_markdown.enable()
-      end,
-    })
-  end,
+  ft = { "markdown", "vimwiki" },
+  opt = function()
+    require ("configs.render-markdown")
+end
 }
--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -5,11 +5,11 @@ return {
     vim.g.vimwiki_list = {
       {
         path = "~/Nextcloud/Notes",
-        syntax = "markdown",
         ext = ".md",
       },
     }
     vim.g.vimwiki_global_ext = 0
+    
   end,
   event = "VeryLazy",
 }
