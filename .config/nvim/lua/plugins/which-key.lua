86ac3c3d961d17357eb84b7de201f307e0f230ed
Marco Castellanos <marcopoulut@gmail.com>
2025-11-11 18:11:46 -0800

    clean up

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,37 +1,29 @@
 -- plugins/which-key.lua
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  cmd = "WhichKey",
-  keys = {
-    { "<leader>", desc = "Leader Key" },
-    { "<c-w>", desc = "Window" },
-    { "\"", desc = "Registers" },
-    { "'", desc = "Marks" },
-    { "`", desc = "Marks" },
-    { "c", mode = { "n", "v" }, desc = "Change" },
-    { "v", mode = { "n" }, desc = "Visual" },
-    { "g", mode = { "n", "v" }, desc = "Goto" },
-  },
-  opts = {
-    plugins = {
-      spelling = true,
-      presets = {
-        operators = false,
-        motions = false,
-        text_objects = false,
-        windows = true,
-        nav = true,
-        z = true,
-        g = true,
-      },
-    },
-    win = {
-      border = "rounded",
-    },
-    layout = {
-      align = "center",
-    },
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	cmd = "WhichKey",
+	keys = {
+		{ "<leader>", desc = "Leader Key" },
+		{ "<c-w>", desc = "Window" },
+		{ '"', desc = "Registers" },
+		{ "'", desc = "Marks" },
+		{ "`", desc = "Marks" },
+		{ "c", mode = { "n", "v" }, desc = "Change" },
+		{ "v", mode = { "n" }, desc = "Visual" },
+		{ "g", mode = { "n", "v" }, desc = "Goto" },
+	},
+	opts = {
+        preset = "helix",
+		plugins = {
+			spelling = true,
+		},
+		win = {
+			border = "rounded",
+		},
+		layout = {
+			width = { min = 20 }, -- min and max width of the columns
+			spacing = 3,
+		},
+	},
 }
-
