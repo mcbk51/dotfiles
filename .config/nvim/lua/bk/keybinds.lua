d7a06922a7669df865fe1b17c32a1b20e45308e8
Marco Castellanos <marcopoulut@gmail.com>
2025-11-12 22:09:34 -0800

    reorder and clean up

--- .config/nvim/lua/bk/keybinds.lua
+++ .config/nvim/lua/bk/keybinds.lua
@@ -5,26 +5,31 @@ local map = function(mode, lhs, rhs, opts)
 	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
---Vim native file system navigator
-map("n", "<leader>pv", vim.cmd.Ex)
-
 -- minifiles
 map("n", "<C-e>", function()
-        local minifiles = require("mini.files")
-        if not minifiles.close() then
-               minifiles.open() 
-        end
-    end, { desc = "toggle mini.files" })
-
--- map("n", "<leader>e", function()
---       require("mini.files").open()
---     end, { desc = "open mini.files" })
+	local minifiles = require("mini.files")
+	if not minifiles.close() then
+		minifiles.open()
+	end
+end, { desc = "toggle mini.files" })
 
 -- Other General changes
+map("n", "<leader>pv", vim.cmd.Ex, {desc = "native file navigator(netrw)" })
 map("n", "<Esc>", "<cmd>noh<CR>", { desc = "general clear highlights" })
-
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "general save file" })
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "general copy whole file" })
+
+map("n", "<leader>u", vim.cmd.UndotreeToggle, {desc = "toggle undo tree"})
+map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
+map("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "make current file executable"})
+
+map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
+
+map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+map("n", "<leader>aa", "<cmd>Alpha <CR>", { desc = "open Alpha" })
+map("n", "<leader>fs", "<cmd>source %<CR>", { desc = "source current file" })
+map("x", "<leader>ph", '"_dP', { desc = "conserve previous highlight" })
 
 -- moving in insert mode
 map("i", "<C-h>", "<Left>", { desc = "move left" })
@@ -37,7 +42,6 @@ map("n", "<c-h>", "<c-w>h", { desc = "switch window left" })
 map("n", "<c-l>", "<c-w>l", { desc = "switch window right" })
 map("n", "<c-j>", "<c-w>j", { desc = "switch window down" })
 map("n", "<c-k>", "<c-w>k", { desc = "switch window up" })
-
 
 -- telescope
 map("n", "<leader>fl", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
@@ -53,7 +57,12 @@ map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git
 map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 --map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
-map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",{ desc = "telescope find all files" })
+map(
+	"n",
+	"<leader>fa",
+	"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+	{ desc = "telescope find all files" }
+)
 map("n", "<leader>fe", function()
 	require("telescope").extensions.file_browser.file_browser({
 		path = vim.fn.expand("%:p:h"),
@@ -76,21 +85,19 @@ map("n", "<leader>x", function()
 	vim.cmd("bdelete")
 end, { desc = "buffer close" })
 
---chmod
-map("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- easymotion
-map("n", "<leader><Leader>s", "<Plug>(easymotion-overwin-f2)", {})
-map("n", "<C-m>", "<Plug>(easymotion-overwin-f2)", {})
-map("n", "<leader><leader>j", "<Plug>(easymotion-j)", {})
-map("n", "<leader><leader>k", "<Plug>(easymotion-k)", {})
+map("n", "<C-m>", "<Plug>(easymotion-overwin-f2)")
+map("n", "<leader>ss","<Plug>(easymotion-overwin-f2)", { desc = "easymotion search 2 characters"})
+map("n", "<leader>sj", "<Plug>(easymotion-j)", { desc = "easymotion jump down"})
+map("n", "<leader>sk", "<Plug>(easymotion-k)", { desc = "easymotion jump up"})
 
 -- vim test
-map("n", "<leader>t", ":TestNearest<CR>")
-map("n", "<leader>T", ":TestFile<CR>")
-map("n", "<leader>a", ":TestSuite<CR>")
-map("n", "<leader>ll", ":TestLast<CR>")
-map("n", "<leader>lv", ":TestVisit<CR>")
+map("n", "<leader>tt", ":TestNearest<CR>", { desc = "Test Nearest" })
+map("n", "<leader>tT", ":TestFile<CR>", { desc = "Test File" })
+map("n", "<leader>ts", ":TestSuite<CR>", { desc = "Test Suite" })
+map("n", "<leader>tl", ":TestLast<CR>", { desc = "Test Last" })
+map("n", "<leader>tv", ":TestVisit<CR>", { desc = "Test Visit" })
 
 -- changing working directory
 map("n", "<leader>cd", function()
@@ -106,21 +113,15 @@ map("n", "<leader>lg", function()
 	vim.cmd("startinsert")
 end, { desc = "Open lazygit in new tab" })
 
--- Comment
-map("n", "<leader>/", "gcc", { desc = "toggle comment", remap = true })
-map("v", "<leader>/", "gc", { desc = "toggle comment", remap = true })
 
 -- Tmux-sessionizer
-map("n", "<C-f>","<cmd>silent !tmux neww ~/dev/scripts/tmux/tmux-sessionizer<CR>",{ desc = "terminal escape terminal mode" })
+map("n","<C-f>","<cmd>silent !tmux neww ~/dev/scripts/tmux/tmux-sessionizer<CR>",{ desc = "terminal escape terminal mode" })
 
--- terminal
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
-
--- Alpha
-map("n", "<C-h>", "<cmd>Alpha <CR>", { desc = "open Alpha" })
-
--- source file
-map("n", "<leader>fs", "<cmd>source %<CR>", { desc = "source current file" })
+-- whichkey
+map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
+map("n", "<leader>wk", function()
+        vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
+end, { desc = "whichkey query lookup" })
 
 -- Move selected text
 map("v", "J", ":m '>+1<CR>gv=gv")
@@ -132,18 +133,4 @@ map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
--- Conserve the previous highlight
-map("x", "<leader>ph", '"_dP', { desc = "conserve previous highlight" })
 
--- whichkey
-map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
-
-map("n", "<leader>wk", function()
-	vim.cmd("WhichKey " .. vim.fn.input("WhichKey: "))
-end, { desc = "whichkey query lookup" })
-
--- undotree
-map("n", "<leader>u", vim.cmd.UndotreeToggle)
-
--- global lsp mappings
-map("n", "<leader>ds", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
