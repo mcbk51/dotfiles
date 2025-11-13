0eb80f6a9544a650d1cdc78a9dea3ca36ea5156d
Marco Castellanos <marcopoulut@gmail.com>
2025-11-12 21:23:18 -0800

    clean up

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -1,35 +1,34 @@
 return {
-  defaults = {
-    prompt_prefix = "   ",
-    selection_caret = " ",
-    entry_prefix = " ",
-    sorting_strategy = "ascending",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-      },
-      width = 0.87,
-      height = 0.80,
-    },
-    mappings = {
-      n = { ["q"] = require("telescope.actions").close },
-    },
-  },
+	defaults = {
+		prompt_prefix = "   ",
+		selection_caret = " ",
+		entry_prefix = " ",
+		sorting_strategy = "ascending",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+			},
+			width = 0.87,
+			height = 0.80,
+		},
+		mappings = {
+			n = { ["q"] = require("telescope.actions").close },
+		},
+	},
+	-- Add this to enable the `project` extension
+	extensions_list = { "themes", "terms", "project" },
 
-  -- Add this to enable the `project` extension
-  extensions_list = { "themes", "terms", "project" },
-
-  extensions = {
-    project = {
-      base_dirs = {
-        { path = "~/dev/projects", max_depth = 2 },
-      },
-      hidden_files = false,
-      theme = "dropdown",
-      order_by = "recent",
-      search_by = "title",
-      sync_with_nvim_tree = false,
-    },
-  },
+	extensions = {
+		project = {
+			base_dirs = {
+				{ path = "~/dev/projects", max_depth = 2 },
+			},
+			hidden_files = false,
+			theme = "dropdown",
+			order_by = "recent",
+			search_by = "title",
+			sync_with_nvim_tree = false,
+		},
+	},
 }
