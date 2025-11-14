ecacf1f20745023cbf086056f2fc480980b2b199
Marco Castellanos <marcopoulut@gmail.com>
2025-11-13 20:38:58 -0800

    removing buffers

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -39,39 +39,28 @@ lualine.setup({
 		lualine_a = { "mode" },
 		lualine_b = {
 			"branch",
+			{ "filename", color = "Normal" },
+		},
+		lualine_c = {},
+		lualine_x = {
+			"encoding",
 			{
-				"buffers",
-				show_modified_status = true,
-				mode = 0,
+				"lsp_status",
+				icon = "", -- f013
 				symbols = {
-					alternate_file = "", -- Text to show to identify the alternate file
-					directory = "", -- Text to show when the buffer is a directory
+					-- Standard unicode symbols to cycle through for LSP progress:
+					spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+					-- Standard unicode symbol for when LSP is done:
+					done = "✓",
+					-- Delimiter inserted between LSP names:
+					separator = " ",
 				},
+				-- List of LSP names to ignore (e.g., `null-ls`):
+				ignore_lsp = {},
+				-- Display the LSP name
+				show_name = false,
 			},
 		},
-		lualine_c = {
-			-- { "filename", path = 1, color = "Normal" },
-		},
-		lualine_x = {
-                "encoding",
-                {
-                        "lsp_status",
-                        icon = '', -- f013
-                        symbols = {
-                                -- Standard unicode symbols to cycle through for LSP progress:
-                                spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
-                                -- Standard unicode symbol for when LSP is done:
-                                done = '✓',
-                                -- Delimiter inserted between LSP names:
-                                separator = ' ',
-                      },
-                      -- List of LSP names to ignore (e.g., `null-ls`):
-                      ignore_lsp = {},
-                      -- Display the LSP name
-                      show_name = false,
-                    }
-                  
-        },
 		lualine_y = { "progress" },
 		lualine_z = {
 			{ "location", color = "Normal" },
