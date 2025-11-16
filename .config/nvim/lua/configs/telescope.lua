b5527a4526e1d0e0c8c77325cab552c093943eb5
Marco Castellanos <marcopoulut@gmail.com>
2025-11-15 20:23:31 -0800

    making prewier smaller

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -1,34 +1,35 @@
 return {
-	defaults = {
-		prompt_prefix = "   ",
-		selection_caret = " ",
-		entry_prefix = " ",
-		sorting_strategy = "ascending",
-		layout_config = {
-			horizontal = {
-				prompt_position = "top",
-				preview_width = 0.55,
-				width = 0.87,
-				height = 0.80,
-			},
-		},
-		mappings = {
-			n = { ["q"] = require("telescope.actions").close },
-		},
-	},
-	-- Add this to enable the `project` extension
-	extensions_list = { "themes", "terms", "project" },
-	pickers = {
-	extensions = {
-		project = {
-			base_dirs = {
-				{ path = "~/dev/projects", max_depth = 2 },
-			},
-			hidden_files = false,
-			theme = "dropdown",
-			order_by = "recent",
-			search_by = "title",
-			sync_with_nvim_tree = false,
-		},
-	},
+  defaults = {
+    prompt_prefix = "   ",
+    selection_caret = " ",
+    entry_prefix = " ",
+    sorting_strategy = "ascending",
+
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.45,
+        width = 0.87,
+        height = 0.80,
+      },
+    },
+    mappings = {
+      n = { ["q"] = require("telescope.actions").close },
+    },
+  },
+  extensions = {
+    project = {
+      base_dirs = {
+        { path = "~/dev/projects", max_depth = 2 },
+      },
+      hidden_files = false,
+      theme = "dropdown",
+      order_by = "recent",
+      search_by = "title",
+      sync_with_nvim_tree = false,
+    },
+  },
+
+  extensions_list = { "themes", "terms", "project" },
 }
+
