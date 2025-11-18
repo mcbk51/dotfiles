4bc734f12aedfe175503cc85ffb743ccf9a1e030
Marco Castellanos <marcopoulut@gmail.com>
2025-11-18 10:32:03 -0800

    adding mini sorround

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,16 @@
+return {
+	"hrsh7th/nvim-cmp",
+	lazy = false,
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lua",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		require("configs.completion")
+	end,
+}
--- .config/nvim/lua/plugins/mini-files.lua
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -1,3 +1,8 @@
 return {
-  "echasnovski/mini.files",
+  "echasnovski/mini.nvim",
+        config = function ()
+               require("mini.files").setup()
+               require("mini.ai").setup()
+               require("mini.surround").setup()
+        end
 }
