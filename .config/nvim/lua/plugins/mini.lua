6666f452722cbebab3a5074d35c5f9e6c35b4065
Marco Castellanos <marcopoulut@gmail.com>
2025-11-19 14:44:14 -0800

    removing surround for now

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,8 +1,7 @@
 return {
-  "echasnovski/mini.nvim",
-        config = function ()
-               require("mini.files").setup()
-               require("mini.ai").setup()
-               require("mini.surround").setup()
-        end
+	"echasnovski/mini.nvim",
+	config = function()
+		require("mini.files").setup()
+		require("mini.ai").setup()
+	end,
 }
