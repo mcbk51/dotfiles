f8aa12073e6eba05df5461281bb2c6c2abdaf8f6
Marco Castellanos <marcopoulut@gmail.com>
2025-11-21 15:45:05 -0800

    adding surround to leader

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -2,10 +2,21 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		require("mini.files").setup({
-                        windows = {
-                                preview = true,
-                        }
-                })
+			windows = {
+				preview = true,
+			},
+		})
 		require("mini.ai").setup()
+		require("mini.surround").setup({
+			mappings = {
+				add = "<leader>sa",
+				delete = "<leader>sd",
+				find = "<leader>sf",
+				find_left = "<leader>sl",
+				highlight = "<leader>sh",
+				replace = "<leader>sr",
+				update_n_lines = "<leader>sn",
+			},
+		})
 	end,
 }
