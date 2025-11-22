addbe3b47322305bb5a391ad14dadbffa50b94d3
Marco Castellanos <marcopoulut@gmail.com>
2025-11-22 15:45:49 -0800

    clean up

--- .config/nvim/lua/plugins/zen.lua
+++ .config/nvim/lua/plugins/zen.lua
@@ -1,36 +1,28 @@
 return {
-  {
-    "folke/zen-mode.nvim",
-    config = function()
-      require("zen-mode").setup {
-        window = {
-          backdrop = 1,
-          height = 0.9,
-          width = 0.8,
-          options = {
-            number = false,
-            relativenumber = false,
-            signcolumn = "no",
-            list = false,
-            cursorline = false,
-          },
-        },
-      }
+	{
+		"folke/zen-mode.nvim",
+		config = function()
+			require("zen-mode").setup({
+				window = {
+					height = 90,
+					options = {},
+				},
+			})
 
-      require("twilight").setup {
-        context = -1,
-        treesitter = true,
-      }
-    end,
-  },
+			require("twilight").setup({
+				context = -1,
+				treesitter = true,
+			})
+		end,
+	},
 
-  {
-    "folke/twilight.nvim",
-    config = function()
-      require("twilight").setup {
-        context = -1,
-        treesitter = true,
-      }
-    end,
-  },
+	{
+		"folke/twilight.nvim",
+		config = function()
+			require("twilight").setup({
+				context = -1,
+				treesitter = true,
+			})
+		end,
+	},
 }
