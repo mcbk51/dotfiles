e7a9a3de22901b8bbe316345d47d4ad3c264d9e9
Marco Castellanos <marcopoulut@gmail.com>
2025-11-21 20:51:53 -0800

    adding wrap

--- .config/nvim/lua/bk/options.lua
+++ .config/nvim/lua/bk/options.lua
@@ -1,17 +1,9 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) texg",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {clear = true}),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.inccommand = "split"
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.smartindent = true
 vim.opt.winborder = "rounded"
@@ -38,11 +30,19 @@ vim.opt.relativenumber = true -- Show relative line numbers
 vim.opt.signcolumn = "yes" -- Keep space on the left for signs (LSP, git, etc.)
 -- vim.opt.numberwidth = 3 -- Width of the number column
 
+--removing ~
+vim.opt.fillchars:append({ eob = " " })
+
 -- Disable broken plugin queries
 vim.g.matchparen_timeout = 20
 vim.g.matchparen_insert_timeout = 20
 
---removing ~
-vim.opt.fillchars:append({ eob = " " })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+        desc = "Highlight when yanking (copying) texg",
+        group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {clear = true}),
+        callback = function()
+                vim.highlight.on_yank()
+        end,
+})
 -- vim.opt.runtimepath:remove(vim.fn.stdpath("data") .. "/lazy/nvim-colo")
