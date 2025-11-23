96a6f3c0bdbd8b6036229ee1e9bade43dd650e5a
Marco Castellanos <marcopoulut@gmail.com>
2025-11-22 22:52:09 -0800

    adding frecency

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,14 @@ return {
 		"nvim-telescope/telescope-file-browser.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-telescope/telescope-smart-history.nvim",
-		{ "nvim-telescope/telescope-frecency.nvim", version = "*" },
+		{
+			"nvim-telescope/telescope-frecency.nvim",
+			version = "*",
+			dependencies = { "kkharji/sqlite.lua" },
+			config = function()
+				require("telescope").load_extension("frecency")
+			end,
+		},
 	},
 	config = function()
 		local telescope = require("telescope")
