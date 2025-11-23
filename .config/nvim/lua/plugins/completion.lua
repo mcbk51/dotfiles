94d32508fea0156b15f9c29b6348c8b86b9c021a
Marco Castellanos <marcopoulut@gmail.com>
2025-11-22 23:03:29 -0800

    clean up

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,7 +1,9 @@
 return {
 	"hrsh7th/nvim-cmp",
 	lazy = false,
+	priority = 100,
 	dependencies = {
+		"onsails/lspkind.nvim",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
