52a487deeebe7c2d9841a56327e9746d5ce49361
Marco Castellanos <marcopoulut@gmail.com>
2025-11-24 20:39:17 -0800

    adding for loop

--- .config/nvim/lua/configs/harpoon.lua
+++ .config/nvim/lua/configs/harpoon.lua
@@ -1,48 +1,44 @@
 local harpoon = require("harpoon")
 local ui = require("harpoon.ui")
 
--- Setup Harpoon v2
 harpoon:setup({
-    settings = {
-        save_on_toggle = true,
-        sync_on_ui_close = true,
-    },
+	settings = {
+		save_on_toggle = true,
+		sync_on_ui_close = true,
+	},
 })
 
--- Telescope extension
 local telescope_ok, telescope = pcall(require, "telescope")
 if telescope_ok then
-    telescope.load_extension("harpoon")
+	telescope.load_extension("harpoon")
 end
 
--- Key mappings for selecting harpoon files
+-- Key mappings
 for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
-    vim.keymap.set("n", string.format("<leader>%d", idx), function()
-        harpoon:list():select(idx)
-    end, { desc = "Harpoon to file " .. idx })
+	vim.keymap.set("n", string.format("<leader>%d", idx), function()
+		harpoon:list():select(idx)
+	end)
 end
 
 -- Add file
 vim.keymap.set("n", "<leader>ha", function()
-    harpoon:list():add()
-end, { desc = "Harpoon add file" })
+	harpoon:list():add()
+end)
 
 vim.keymap.set("n", "<m-a>", function()
-    harpoon:list():add()
-    vim.notify("File added to Harpoon", "info", { title = "Harpoon" })
-end, { desc = "Harpoon add file" })
+	harpoon:list():add()
+	vim.notify("File added to Harpoon", "info", { title = "Harpoon" })
+end)
 
--- Toggle quick menu (FIXED)
 vim.keymap.set("n", "<leader>hh", function()
-    ui.toggle_quick_menu(harpoon:list())
-end, { desc = "Harpoon quick menu" })
+	harpoon.ui:toggle_quick_menu(harpoon:list(), {})
+end)
 
 vim.keymap.set("n", "<m-h>", function()
-    ui.toggle_quick_menu(harpoon:list())
-end, { desc = "Harpoon quick menu" })
+	harpoon.ui:toggle_quick_menu(harpoon:list(), {})
+end)
 
--- Remove current file
+-- Remove file
 vim.keymap.set("n", "<leader>hr", function()
-    harpoon:list():remove()
-end, { desc = "Harpoon remove file" })
-
+	harpoon:list():remove()
+end)
