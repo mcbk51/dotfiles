0c9933a83fa095c3749dded967dbab4ce8466dab
Marco Castellanos <marcopoulut@gmail.com>
2025-11-24 20:33:24 -0800

    adding for loop

--- .config/nvim/lua/configs/harpoon.lua
+++ .config/nvim/lua/configs/harpoon.lua
@@ -1,77 +1,48 @@
 local harpoon = require("harpoon")
 local ui = require("harpoon.ui")
 
--- Setup harpoon with settings from file 1
+-- Setup Harpoon v2
 harpoon:setup({
-	settings = {
-		save_on_toggle = true,
-		sync_on_ui_close = true,
-	},
+    settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+    },
 })
 
--- Load telescope extension if telescope is available
+-- Telescope extension
 local telescope_ok, telescope = pcall(require, "telescope")
 if telescope_ok then
-	telescope.load_extension("harpoon")
+    telescope.load_extension("harpoon")
 end
 
--- Safe file opening function from file 2
-local function open_harpoon_file(n)
-	local ok, err = pcall(function()
-		local file = harpoon:list():get(n)
-		if file then
-			vim.cmd("edit " .. vim.fn.fnameescape(file.value))
-		else
-			vim.notify("No Harpoon file at index " .. n, vim.log.levels.WARN)
-		end
-	end)
-	if not ok then
-		vim.notify("Failed to open Harpoon file " .. n .. ": " .. err, vim.log.levels.ERROR)
-	end
+-- Key mappings for selecting harpoon files
+for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+    vim.keymap.set("n", string.format("<leader>%d", idx), function()
+        harpoon:list():select(idx)
+    end, { desc = "Harpoon to file " .. idx })
 end
 
--- Key mappings from file 2
-vim.keymap.set("n", "<leader>1", function()
-	open_harpoon_file(1)
-end, { desc = "Harpoon to file 1" })
-
-vim.keymap.set("n", "<leader>2", function()
-	open_harpoon_file(2)
-end, { desc = "Harpoon to file 2" })
-
-vim.keymap.set("n", "<leader>3", function()
-	open_harpoon_file(3)
-end, { desc = "Harpoon to file 3" })
-
-vim.keymap.set("n", "<leader>4", function()
-	open_harpoon_file(4)
-end, { desc = "Harpoon to file 4" })
-
-vim.keymap.set("n", "<leader>5", function()
-	open_harpoon_file(5)
-end, { desc = "Harpoon to file 5" })
-
--- Additional harpoon keybinds
 -- Add file
 vim.keymap.set("n", "<leader>ha", function()
-	harpoon:list():add()
+    harpoon:list():add()
 end, { desc = "Harpoon add file" })
 
 vim.keymap.set("n", "<m-a>", function()
-	harpoon:list():add()
-	vim.notify("File added to harpoon", "info", { title = "Harpoon" })
+    harpoon:list():add()
+    vim.notify("File added to Harpoon", "info", { title = "Harpoon" })
 end, { desc = "Harpoon add file" })
 
--- Toggle quick menu
+-- Toggle quick menu (FIXED)
 vim.keymap.set("n", "<leader>hh", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
+    ui.toggle_quick_menu(harpoon:list())
 end, { desc = "Harpoon quick menu" })
 
 vim.keymap.set("n", "<m-h>", function()
-	harpoon.ui:toggle_quick_menu(harpoon:list())
+    ui.toggle_quick_menu(harpoon:list())
 end, { desc = "Harpoon quick menu" })
 
 -- Remove current file
 vim.keymap.set("n", "<leader>hr", function()
-	harpoon:list():remove()
+    harpoon:list():remove()
 end, { desc = "Harpoon remove file" })
+
