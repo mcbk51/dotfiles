f97de80b1151b70259ca4ca197ae53dc994b608e
Marco Castellanos <marcopoulut@gmail.com>
2025-11-26 15:35:36 -0800

    adding color to zenmode

--- .config/nvim/lua/bk/themes.lua
+++ .config/nvim/lua/bk/themes.lua
@@ -58,3 +58,11 @@ require("onedark").setup({
 })
 
 vim.cmd("colorscheme onedark")
+
+function ColorMyPencils(color)
+	color = color or "onedark"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
