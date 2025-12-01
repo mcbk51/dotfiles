6db9e383a3dbedcdec3af77222ae2650be71e0a4
Marco Castellanos <marcopoulut@gmail.com>
2025-11-30 20:56:33 -0800

    adding priority to fix bug

--- .config/nvim/lua/plugins/transparent-nvim.lua
+++ .config/nvim/lua/plugins/transparent-nvim.lua
@@ -1,5 +1,6 @@
 return {
 	"xiyaowong/transparent.nvim",
+	priority = 1000,
 	config = function()
 		local transparent = require("transparent")
 
@@ -32,18 +33,40 @@ return {
 				"PmenuThumb",
 
 				-- Lualine
-				"lualine_b_normal", "lualine_b_insert", "lualine_b_visual",
-				"lualine_b_command", "lualine_b_replace", "lualine_b_terminal",
-				"lualine_c_normal", "lualine_c_insert", "lualine_c_visual",
-				"lualine_c_command", "lualine_c_replace", "lualine_c_terminal",
-				"lualine_x_normal", "lualine_x_insert", "lualine_x_visual",
-				"lualine_x_command", "lualine_x_replace", "lualine_x_terminal",
-				"lualine_y_normal", "lualine_y_insert", "lualine_y_visual",
-				"lualine_y_command", "lualine_y_replace", "lualine_y_terminal",
-				"lualine_z_normal", "lualine_z_insert", "lualine_z_visual",
-				"lualine_z_command", "lualine_z_replace", "lualine_z_terminal",
-				"lualine_c_inactive", "lualine_x_inactive",
-				"lualine_y_inactive", "lualine_z_inactive",
+				"lualine_b_normal",
+				"lualine_b_insert",
+				"lualine_b_visual",
+				"lualine_b_command",
+				"lualine_b_replace",
+				"lualine_b_terminal",
+				"lualine_c_normal",
+				"lualine_c_insert",
+				"lualine_c_visual",
+				"lualine_c_command",
+				"lualine_c_replace",
+				"lualine_c_terminal",
+				"lualine_x_normal",
+				"lualine_x_insert",
+				"lualine_x_visual",
+				"lualine_x_command",
+				"lualine_x_replace",
+				"lualine_x_terminal",
+				"lualine_y_normal",
+				"lualine_y_insert",
+				"lualine_y_visual",
+				"lualine_y_command",
+				"lualine_y_replace",
+				"lualine_y_terminal",
+				"lualine_z_normal",
+				"lualine_z_insert",
+				"lualine_z_visual",
+				"lualine_z_command",
+				"lualine_z_replace",
+				"lualine_z_terminal",
+				"lualine_c_inactive",
+				"lualine_x_inactive",
+				"lualine_y_inactive",
+				"lualine_z_inactive",
 			},
 		})
 
@@ -52,4 +75,3 @@ return {
 		transparent.clear_prefix("WhichKey")
 	end,
 }
-
