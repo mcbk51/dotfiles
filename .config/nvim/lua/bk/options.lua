2efb297ce7be5d655c1a0df836b3a13e480790f3
Marco Castellanos <marcopoulut@gmail.com>
2025-11-30 17:15:17 -0800

    clean up

--- .config/nvim/lua/bk/options.lua
+++ .config/nvim/lua/bk/options.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 
 local opt = vim.opt
+local api = vim.api
+
 
 opt.tabstop = 4
 opt.softtabstop = 4
@@ -41,11 +43,10 @@ opt.signcolumn = "yes" -- Keep space on the left for signs (LSP, git, etc.)
 opt.fillchars:append({ eob = " " })
 
 -- Disable broken plugin queries
-vim.g.matchparen_timeout = 20
-vim.g.matchparen_insert_timeout = 20
+-- vim.g.matchparen_timeout = 20
+-- vim.g.matchparen_insert_timeout = 20
 
-
-vim.api.nvim_create_autocmd("TextYankPost", {
+api.nvim_create_autocmd("TextYankPost", {
         desc = "Highlight when yanking (copying) texg",
         group = vim.api.nvim_create_augroup("kickstart-highlight-yank", {clear = true}),
         callback = function()
@@ -54,14 +55,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Show notification when recording starts/stops
-vim.api.nvim_create_autocmd("RecordingEnter", {
+api.nvim_create_autocmd("RecordingEnter", {
   callback = function()
     local reg = vim.fn.reg_recording()
     vim.notify("Recording macro to register: " .. reg, vim.log.levels.INFO)
   end,
 })
 
-vim.api.nvim_create_autocmd("RecordingLeave", {
+api.nvim_create_autocmd("RecordingLeave", {
   callback = function()
     vim.notify("Macro recording stopped", vim.log.levels.INFO)
   end,
