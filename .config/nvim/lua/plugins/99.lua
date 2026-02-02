139404f8a6517fd311534651eca55e5c7cabd97f
Marco Castellanos <marcopoulut@gmail.com>
2026-02-02 12:30:50 -0800

    fixing prompt issue

--- .config/nvim/lua/plugins/99.lua
+++ .config/nvim/lua/plugins/99.lua
@@ -63,9 +63,7 @@ return {
 			})
 
             -- Create your own short cuts for the different types of actions
-			vim.keymap.set("n", "<leader>9f", function()
-				_99.fill_in_function()
-			end)
+
             -- take extra note that i have visual selection only in v mode
             -- technically whatever your last visual selection is, will be used
             -- so i have this set to visual mode so i dont screw up and use an
@@ -73,14 +71,10 @@ return {
             --
             -- likely ill add a mode check and assert on required visual mode
             -- so just prepare for it now
-			vim.keymap.set("v", "<leader>9v", function()
-				_99.visual()
-			end)
+
 
             --- if you have a request you dont want to make any changes, just cancel it
-			vim.keymap.set("v", "<leader>9s", function()
-				_99.stop_all_requests()
-			end)
+
 
             --- Example: Using rules + actions for custom behaviors
             --- Create a rule file like ~/.rules/debug.md that defines custom behavior.
